a19002c11ffe48461b6793ba2908b425b7b58b51
NomadNiko <nomad.niko.inc@gmail.com>
2023-03-05 05:16:44 -0700

    test

--- script.js
+++ script.js
@@ -15,8 +15,8 @@ function login(event) {
 	xhr.onload = function() {
 		if (xhr.status === 200) {
 			alert('Logged in successfully!');
-			document.location.href = '/dashboard';
-			// redirect to dashboard or homepage
+			document.location.href = '/test_auth.html';
+			// redirect to test_auth
 		} else {
 			alert('Error: ' + xhr.statusText);
 		}
